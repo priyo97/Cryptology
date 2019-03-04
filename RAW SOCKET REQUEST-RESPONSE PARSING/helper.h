2e25a42061beb28e0bdc18acb4af2d37afb55bd6
Priyabrata Saha <sahapriyabrata97@gmail.com>
2019-03-04 16:13:20 +0530

    Initial commit

---
+++ RAW SOCKET REQUEST-RESPONSE PARSING/helper.h
@@ -0,0 +1,285 @@
+// contains helper functions related to raw sockets
+// functions used from libraries
+/*
+
+    stdio.h
+    string.h
+    sys/socket.h
+    sys/types.h
+    net/if.h
+    linux/if_packet.h
+
+*/
+
+
+struct header_fields{
+
+    unsigned char **keys;
+    unsigned char **values;
+    int num_headers;
+};
+
+struct form_field{
+
+    unsigned char *name;
+    short is_file;
+    unsigned char *filename;
+    unsigned char  *value;
+    int length;
+};
+
+struct form_data{
+
+    struct form_field *f;
+    int num_form_fields;
+
+};
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+
+int create_raw_socket(char *interface, unsigned char protocol)
+{
+    int sock = socket(AF_PACKET, SOCK_RAW, htons(protocol));
+
+    if(sock == -1)
+    {
+        error("Failed to create socket");    
+    }
+    else
+    {
+        printf("Socket successfully created\n");
+    }
+        
+    
+    struct sockaddr_ll socket_address;
+
+    bzero((char*)&socket_address, sizeof(socket_address));
+
+
+
+
+    socket_address.sll_family = AF_PACKET;
+
+    socket_address.sll_ifindex = if_nametoindex(interface);
+
+    socket_address.sll_protocol = htons(protocol);
+
+
+
+    int check = bind(sock, (struct sockaddr*) &socket_address, sizeof(socket_address));
+
+
+    if(check < 0)
+
+        error("Binding Error");
+
+
+    return sock;
+
+}
+
+
+char* get_header_value(char *searchkey, struct header_fields *http_hdr)
+{
+    
+    register int i;
+
+    for(i = 0 ; i < http_hdr -> num_headers ; i++)
+    {
+        if( strcmp( searchkey, http_hdr -> keys[i] ) == 0 )
+        {
+            return http_hdr -> values[i];
+        }
+        
+    }
+    
+    return NULL;
+}
+
+
+
+
+void extract_header_fields(char *header_portion, int header_len, struct header_fields *http_hdr)
+{
+    unsigned char **headers = malloc(100 * sizeof(unsigned char*));
+
+
+    // num_headers to keep track of number of headers
+    // j to keep track of the beginning of the header
+    register int i, j, num_headers;
+
+    
+    num_headers = j = 0;
+
+
+    for(i = 0 ; i < header_len ; i++ )
+    {
+        if(header_portion[i] == '\r')
+        {
+            
+            header_portion[i] = '\0';
+
+            headers[num_headers] = header_portion + j;
+
+            j = i + 2;
+
+            num_headers++ ;
+
+            i++;
+        }
+    }
+
+    unsigned char **keys   = malloc( (num_headers - 1) * sizeof(unsigned char *));
+    unsigned char **values = malloc( (num_headers - 1) * sizeof(unsigned char *));
+
+    unsigned char *tmp;
+
+    for(i = 1 ; i < num_headers ; i++)
+    {
+        tmp = headers[i];
+    
+        j = 0;
+
+        while(tmp[j] != ':')
+        {
+            j++;
+        }
+
+        tmp[j] = '\0';
+    
+        // i - 1 cause i is starting from 1 to avoid the HTTP response code stored at index 0 of headers and I want to insert values from index 0
+
+        keys[i-1] = tmp;
+        values[i-1] = (tmp + j + 2);
+    }
+
+    
+    free(headers);
+
+    
+
+    http_hdr -> keys = keys;
+    http_hdr -> values = values;
+    http_hdr -> num_headers = num_headers - 1;
+
+}
+
+
+
+void extract_form_fields(char* header_portion,int header_len,char *body_portion,int body_len, struct form_data *f)
+{
+    unsigned char *tmp;
+
+    struct header_fields http_hdr;
+
+    extract_header_fields(header_portion, header_len, &http_hdr);
+
+    tmp = get_header_value("Content-Type", &http_hdr);
+
+    tmp = strstr(tmp,"=") + 1;
+
+
+
+
+    unsigned char delimiter[100] = "--";
+
+    strcat( (char*) delimiter, (const char*) tmp );
+
+
+    size_t delimiter_len = strlen(delimiter);
+
+    tmp = body_portion;
+
+
+
+
+
+    unsigned char *token, *t, *s, *r, *q;
+
+    int i = 0;
+
+    struct form_field *form_data = malloc(20 * sizeof(struct form_field));
+
+    while(1)
+    {
+        token = (unsigned char*)memmem( tmp, body_len, delimiter, strlen(delimiter) );
+
+        if(token == NULL)
+        {
+            break;
+        }
+
+        token[0] = '\0';
+
+        if( strcmp(tmp,"") != 0 )
+        {
+            t = strstr(tmp,"\r\n\r\n");
+
+            t[0] = '\0';
+
+            t = t + 4;
+
+            s = strstr(tmp,"filename=\"");
+
+            if(s != NULL)
+            {
+                r = strstr(tmp,"name=\"");
+
+                q = strstr(r + 6,"\"");
+
+                q[0] = '\0';
+        
+                q = strstr(s + 10,"\"");
+
+                q[0] = '\0';
+
+                
+                form_data[i].name = (r + 6);
+                form_data[i].is_file = 1;
+                form_data[i].filename = (s + 10);
+                form_data[i].value = t;
+                form_data[i].length = token - t - strlen("\r\n");   
+            }
+            else
+            {
+                r = strstr(tmp,"name=\"");
+
+                q = strstr(r + 6,"\"");
+
+                q[0] = '\0';
+
+                form_data[i].name = (r + 6);
+                form_data[i].is_file = 0;
+                form_data[i].filename = NULL;
+                form_data[i].value = t;
+                form_data[i].length = token - t - strlen("\r\n");
+
+            }
+
+            i++;
+        }
+
+        body_len -= (token - tmp);
+        tmp = token + strlen(delimiter) + strlen("\r\n");
+        body_len -= strlen(delimiter) + strlen("\r\n");
+    }
+
+
+    free(http_hdr.keys);
+    free(http_hdr.values);
+
+
+
+  //  printf("\n##############################################\n");
+  //  printf("Name    : %s\n",form_data[2].name);
+  //  printf("Filename: %s\n",form_data[2].filename);
+  //  printf("%s\n",form_data[2].value);
+
+    f -> f = form_data;
+    f -> num_form_fields = i;
+}
---
+++ RAW SOCKET REQUEST-RESPONSE PARSING/rawreq.c
@@ -0,0 +1,219 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<net/if.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<linux/if_ether.h>
+#include<linux/if_packet.h>
+#include<linux/ip.h>
+#include<linux/tcp.h>
+#include"helper.h"
+
+#define MODE "w"
+
+int main() 
+{    
+    int sock = create_raw_socket("eth1",ETH_P_ALL);
+	
+
+    unsigned char buffer[65536];
+
+	ssize_t recv_size = -1;
+
+
+
+
+    int data_start = 0;
+
+    int header_len, body_len, i, header_found = 0, body_found = 0;
+
+    unsigned char *tmp, *header_portion, *body_portion;
+
+    
+
+
+    header_portion = (unsigned char*) malloc(65536 * sizeof(unsigned char));
+
+    body_portion = (unsigned char*) malloc(65536 * sizeof(unsigned char));
+
+    header_len = body_len = 0;
+	
+
+    while(1)
+	{
+		bzero(&buffer,sizeof(buffer));
+        	
+		recv_size = recv(sock, &buffer,65536, 0);
+        	
+
+		if (recv_size < 1)
+        {
+            break;
+		}
+
+
+		struct ethhdr *eth = (struct ethhdr *)(buffer);
+
+		struct iphdr *ip = (struct iphdr *)(buffer + sizeof(struct ethhdr));
+
+
+
+		struct sockaddr_in source, dest;
+
+		
+	    bzero(&source, sizeof(source));
+		bzero(&dest, sizeof(dest));
+
+
+		source.sin_addr.s_addr = ip -> saddr;
+		dest.sin_addr.s_addr = ip -> daddr; 
+
+
+        struct tcphdr *tcp = (struct tcphdr*)( buffer + sizeof(*ip) + sizeof(*eth));
+
+
+	    if( strcmp(inet_ntoa(source.sin_addr),"192.168.4.159") == 0 && tcp -> fin )
+        {
+            break;
+        }
+
+
+	    if( strcmp(inet_ntoa(dest.sin_addr),"192.168.4.159") == 0 )
+	    {
+
+
+		    unsigned char *data = (unsigned char*)(buffer + sizeof(*tcp) + sizeof(*ip) + sizeof(*eth));
+		
+            int len = recv_size - sizeof(*tcp) - sizeof(*ip) - sizeof(*eth);
+
+
+            if(!data_start && data[0] == 'P')
+                
+                data_start = 1;
+
+
+            if(data_start)
+            {
+
+                if(!header_found)
+                {
+
+
+
+                    header_portion = (unsigned char*) realloc(header_portion, strlen(header_portion) + 65536 * sizeof(unsigned char));
+
+                    tmp = strstr(data,"\r\n\r\n");
+
+                    if(tmp != NULL)
+                    {
+                        tmp[2] = '\0';
+
+                        tmp = tmp + 4;
+
+        
+
+                        strcpy(header_portion + header_len, data);
+
+                        header_len += strlen(data);
+
+                        header_found = 1;
+
+
+
+
+
+                        len = len - strlen(data) - 2;
+
+
+                        
+                        for( i = 0 ; i < len ; i++)
+                        {
+                            body_portion[body_len++] = tmp[i];
+                        }
+                        
+
+                        body_found = 1;
+        
+
+                    }
+                    else
+                    {
+                        strcpy(header_portion + header_len, data);
+                        header_len += strlen(data);
+                    }            
+
+
+
+                }
+                else if(body_found)
+                {
+                   
+
+                    if( strcmp(data,"") == 0 || tcp -> fin )
+                    {
+                        break;
+                    }
+
+
+                    body_portion = (unsigned char *) realloc(body_portion, strlen(body_portion) + 65536  * sizeof(unsigned char));
+        
+
+                    for( i = 0 ; i < len ; i++)
+                    {
+                        body_portion[body_len++] = data[i];
+                    }
+
+
+
+                }
+
+            }
+		
+    	}
+
+    }
+
+
+    //printf("Head-portion:\n\n%s\n", header_portion);
+    
+    //printf("Body-portion:\n");
+
+    struct form_data data;
+
+    extract_form_fields(header_portion, header_len, body_portion, body_len, &data);
+
+
+
+    //printf("%d\n",data.num_form_fields);
+
+    for(i = 0 ; i < data.num_form_fields ; i++)
+    {
+        
+        if(data.f[i].is_file)
+        {
+            FILE *fptr = fopen(data.f[i].filename,MODE);
+
+            if(fptr == NULL)
+                
+                error("Error with file opening");
+                
+            fwrite(data.f[i].value, sizeof(unsigned char), data.f[i].length, fptr);
+
+            fflush(fptr);
+            fclose(fptr);
+        }
+    }
+
+
+
+    free(data.f);    
+    
+    free(header_portion);
+    free(body_portion);
+
+
+    close(sock);
+}
